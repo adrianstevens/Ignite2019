9152a02beb72e824a2800ff16ed0bb1b065a57f8
adrianstevens <adrian.stevens@gmail.com>
2019-10-24 08:26:37 -0700

    Initial commit

---
+++ ConsoleTestApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+
+namespace ConsoleTestApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Bitmap map = new Bitmap("");
+
+            map.W
+
+
+            new Bitmap(0, 0, System.Drawing.Imaging.PixelFormat.Format16bppArgb1555);
+        }
+    }
+}
---
+++ MeadowHandheldDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using Meadow;
+
+namespace BasicMeadowHandheldDemo
+{
+    class Program
+    {
+        static IApp app;
+        public static void Main(string[] args)
+        {
+            // instantiate and run new meadow app
+            app = new App();
+        }
+    }
+}
